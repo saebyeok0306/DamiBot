de836b8accddaefa6efdc424e6adc4c066d27b35
JangSeHun <westreed@naver.com>
2024-08-21 22:13:24 +0900

    feat: deploy

--- script/start.sh
+++ script/start.sh
@@ -6,4 +6,6 @@ if screen -list | grep -q "\.dami"; then
   screen -S $SCREEN_SESSION -X quit
 fi
 
-screen -dS $SCREEN_SESSION python3 app.py
+source .venv/bin/activate
+pip install -r requirements.txt
+screen -dS $SCREEN_SESSION python app.py
