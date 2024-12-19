36afdf603256c001bdf3f77fc86939bf3b9f5349
JangSeHun <westreed@naver.com>
2024-12-19 20:46:20 +0900

    feat: Add method to compare if two Music objects are the same

--- db/model/Music.py
+++ db/model/Music.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from sqlalchemy import String, Column, Integer
 
 from db.Database import Base
@@ -13,11 +15,12 @@ class Music(Base):
     music_dlc = Column(String(200))
     music_nickname = Column(String(200))
 
-    def __init__(self, music_name, music_artist, music_bpm, music_dlc):
+    def __init__(self, music_name, music_artist, music_bpm, music_dlc, music_nickname=None):
         self.music_name = music_name
         self.music_artist = music_artist
         self.music_bpm = music_bpm
         self.music_dlc = music_dlc
+        self.music_nickname = music_nickname
 
     def __str__(self):
         return f"Music({self.id}, {self.music_name}, {self.music_artist}, {self.music_bpm}, {self.music_dlc})"
@@ -26,3 +29,21 @@ class Music(Base):
         document = self.__dict__
         document["content"] = f"{self.music_name.lower()} {self.music_artist.lower()} {self.music_nickname}"
         return document
+
+    def __eq__(self, other: Music):
+        if self.music_name != other.music_name:
+            return False
+        if self.music_dlc != other.music_dlc:
+            return False
+        if self.music_artist != other.music_artist:
+            return False
+        return True
+
+    def is_update(self, other: Music):
+        if not self.__eq__(other):
+            return False
+
+        if self.music_nickname != other.music_nickname:
+            return True
+
+        return False
